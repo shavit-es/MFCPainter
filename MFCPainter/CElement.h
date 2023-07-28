532db4747e6d2227dbf4358231e50382b8f14812
shavit0423 <shavit0423@naver.com>
2023-07-28 18:04:53 +0900

    7.28마무리커밋

--- MFCPainter/CElement.h
+++ MFCPainter/CElement.h
@@ -4,6 +4,11 @@
 class CElement :public CObject
 {
 private:
+	LONG x, y, xw, yh;
+	int linethickness;
+	COLORREF linecolor, fillcolor;
+	CPoint point;
+	bool bline;
 
 public:
 	CElement::CElement();
@@ -11,24 +16,33 @@ public:
 	CElement(CElement& other);
 
 	CPoint Getpoint() {
+		return point;
 	}
 	bool Getbline() {
+		return bline;
 	}
 	void Draw(CDC &memCDC) {
 	}
 	LONG Getx() {
+		return x;
 	}
 	LONG Gety() {
+		return y;
 	}
 	LONG Getxw() {
+		return xw;
 	}
 	LONG Getyh() {
+		return yh;
 	}
 	int Getlinethickness() {
+		return linethickness;
 	}
 	COLORREF Getlinecolor() {
+		return linecolor;
 	}
 	COLORREF Getfillcolor() {
+		return fillcolor;
 	}
 };
 
